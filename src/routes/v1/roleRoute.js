32c2e0e21a6fdb073f12f43afc93fa8702b7f14c
Asmodeous <105940384+Siddhant02082002@users.noreply.github.com>
2024-09-01 10:31:53 +0530

    Update roleRoute.js

--- src/routes/v1/roleRoute.js
+++ src/routes/v1/roleRoute.js
@@ -7,15 +7,15 @@ import roleController from '~/controllers/roleController';
 
 const router = Router();
 
-router.get('/', authenticate('role:read'), validate(roleValidation.getRoles), catchAsync(roleController.getRoles));
-router.post('/', authenticate('role:create'), validate(roleValidation.createRole), catchAsync(roleController.createRole));
-router.get('/:roleId', authenticate('role:read'), validate(roleValidation.getRole), catchAsync(roleController.getRole));
-router.put('/:roleId', authenticate('role:update'), validate(roleValidation.updateRole), catchAsync(roleController.updateRole));
-router.delete(
-	'/:roleId',
-	authenticate('role:delete'),
-	validate(roleValidation.deleteRole),
-	catchAsync(roleController.deleteRole)
-);
+// router.get('/', authenticate('role:read'), validate(roleValidation.getRoles), catchAsync(roleController.getRoles));
+// router.post('/', authenticate('role:create'), validate(roleValidation.createRole), catchAsync(roleController.createRole));
+// router.get('/:roleId', authenticate('role:read'), validate(roleValidation.getRole), catchAsync(roleController.getRole));
+// router.put('/:roleId', authenticate('role:update'), validate(roleValidation.updateRole), catchAsync(roleController.updateRole));
+// router.delete(
+// 	'/:roleId',
+// 	authenticate('role:delete'),
+// 	validate(roleValidation.deleteRole),
+// 	catchAsync(roleController.deleteRole)
+// );
 
 export default router;
